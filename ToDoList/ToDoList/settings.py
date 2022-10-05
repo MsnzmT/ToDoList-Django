b4d7d3bfe99cb16d584dfb5325039c14e1fb0f72
Mohsen Zahmatkesh <msn.zmt81@gmail.com>
2022-10-05 15:12:37 +0330

    fix some configurations of project

--- ToDoList/ToDoList/settings.py
+++ ToDoList/ToDoList/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
